0bd5eb3506dd2a45c8adff2f432a4afb52dab081
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-05-22 04:09:29 +0100

    finish design and some functionalities

--- main/urls.py
+++ main/urls.py
@@ -2,14 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
+    path("register", views.register_view, name="register"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
     path("", views.home, name="home"),
     path("room/<str:pk>", views.room, name="room"),
-    path("profile/<str:pk>/", views.user_profile, name="user-profile"),
-    path("create-room/", views.create_room, name="create-room"),
-    path("update-room/<str:pk>/", views.update_room, name="update-room"),
-    path("delete-room/<str:pk>/", views.delete_room, name="delete-room"),
-    path("delete-message/<str:pk>/", views.delete_message, name="delete-message"),
+    path("profile/<str:pk>", views.user_profile, name="user-profile"),
+    path("create-room", views.create_room, name="create-room"),
+    path("update-room/<str:pk>", views.update_room, name="update-room"),
+    path("delete-room/<str:pk>", views.delete_room, name="delete-room"),
+    path("delete-message/<str:pk>", views.delete_message, name="delete-message"),
+    path("update-user", views.update_user, name="update-user"),
+    path('topics', views.topics_page, name='topics'),
 ]
--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 def home(request):
@@ -20,7 +20,7 @@ def home(request):
         | Q(host__username=q)
     )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[:5]
     chats = Message.objects.filter(Q(room__topic__name__icontains=q))
     context = {
         "rooms": rooms,
@@ -48,16 +48,19 @@ def room(request, pk):
 @login_required(login_url="/login")
 def create_room(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            room.participants.add(request.user)
-            room.save()
-            return redirect("room", pk=room.id)
-    context = {"form": form}
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room = Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get("name"),
+            description=request.POST.get("description"),
+        )
+        room.participants.add(request.user)
+        return redirect("room", pk=room.id)
+    context = {"form": form, "topics": topics}
     return render(request, "main/room_form.html", context)
 
 
@@ -65,22 +68,25 @@ def create_room(request):
 def update_room(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse("You don't have the permission to do this! ")
     if request.method == "POST":
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect("room", pk=room.id)
-    context = {"form": form}
+        topic_name = request.POST.get("topic")
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get("name")
+        room.topic = topic
+        room.description = request.POST.get("description")
+        room.save()
+        return redirect("room", pk=room.id)
+    context = {"form": form, "topics": topics, "room": room}
     return render(request, "main/room_form.html", context)
 
 
 @login_required(login_url="/login")
 def delete_room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {"room": room}
+    context = {"obj": room, "type": "room"}
 
     if request.user != room.host:
         return HttpResponse("You don't have the permission to do this! ")
@@ -138,7 +144,7 @@ def register_view(request):
 
 def delete_message(request, pk):
     chat = Message.objects.get(id=pk)
-    context = {"chat": chat}
+    context = {"obj": chat, "type": "message"}
 
     if request.user != chat.user:
         return HttpResponse("You don't have the permission to do this! ")
@@ -164,3 +170,23 @@ def user_profile(request, pk):
         "rooms_count": all_rooms.count(),
     }
     return render(request, "main/profile.html", context)
+
+
+@login_required(login_url="login")
+def update_user(request):
+    user = request.user
+    form = UserForm(instance=user)
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect("user-profile", pk=user.id)
+    return render(request, "main/update-user.html", {"form": form})
+
+
+def topics_page(request):
+    q = request.GET.get("q")
+    q = q if q else ""
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {"topics": topics}
+    return render(request, "main/topics.html", context)
